b9309c8178f3f7a05e6c16f9e21204c057b4eeb9
Guillermo <guinala@alumni.uv.es>
2026-02-26 10:52:42 +0100

    Estructura base de carpetas e inicializador de Firebase

---
+++ src/services/firebase_init.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD49g9ymjjH8jZQmlNk8dwBwr4ev8NQuWU",
+  authDomain: "bookproject-c1fa0.firebaseapp.com",
+  projectId: "bookproject-c1fa0",
+  storageBucket: "bookproject-c1fa0.firebasestorage.app",
+  messagingSenderId: "458729035046",
+  appId: "1:458729035046:web:0a384df765e195a0e1dde7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
