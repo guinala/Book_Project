d912bded2c735cc8709719b736ea3117be3dafc1
guinala <guinala@alumni.uv.es>
2026-03-09 16:28:34 +0100

    Instalación Capacitor y React I18Next. Cambio en SCSS para ser mobile-first

---
+++ android/app/src/main/java/com/id3/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.id3.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.id3.app',
+  appName: 'book-project',
+  webDir: 'dist'
+};
+
+export default config;
