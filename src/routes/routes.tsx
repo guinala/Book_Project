4ac3d6dd9efd9e17a26ae54675c57999329f6c62
guinala <guinala@alumni.uv.es>
2026-05-20 00:52:07 +0200

    Pagina de detalle de lista y conexión con otras páginas

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -12,6 +12,7 @@ import EditProfilePage from "@/pages/edit-profile/EditProfilePage";
 import SettingsPage from "@/pages/settings/SettingsPage";
 import AuthRoute from "@/routes/AuthRoute";
 import AllListsPage from "@/pages/lists/AllListsPage";
+import ListDetailPage from "@/pages/lists/ListDetailPage";
 
 export const ROUTES = [
   {
@@ -66,6 +67,9 @@ export const ROUTES = [
       },
       { path: "lists/:userId", 
         element: <AllListsPage /> 
+      },
+      { path: "lists/:userId/:listId", 
+        element: <ListDetailPage /> 
       },
       {
         path: "settings",
